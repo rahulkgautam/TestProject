0a363c2f6add3f0c04da5f37d76c18ee1187b8ce
rahulkgautam <131359869+rahulkgautam@users.noreply.github.com>
2023-10-13 01:56:18 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,34 @@
+﻿using LoginLogout.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginLogout.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Save(Employee details)
+        {
+
+            return Json(true);
+        }
+        public ActionResult GetEmployee(Employee details)
+        {
+            var data = new Employee()
+            {
+                Desibility = true,
+                FirstName="Rahul",
+                LastName="Kumar"
+            };
+            return Json(data);
+        }
+    }
+}
---
+++ LoginController.cs
@@ -0,0 +1,35 @@
+﻿using LoginLogout.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace LoginLogout.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult LoginPage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Users users)
+        {
+            if (users.UserName == "Rahul123" && users.Password == "Rahul12345@")
+            {
+                FormsAuthentication.SetAuthCookie("Rahul123",false);
+                return RedirectToAction("Index","Home");
+            }
+            return View("LoginPage");
+        }
+        public ActionResult Logout()
+        {
+             FormsAuthentication.SignOut();
+            return RedirectToAction("LoginPage","Login");
+        }
+
+    }
+}
