236ccabd3528bd0fcfe9a0cd054159bced257d29
rahulkgautam <131359869+rahulkgautam@users.noreply.github.com>
2023-10-13 02:00:48 +0530

    Add files via upload

---
+++ Connection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CrupAppNew
+{
+    public class Connection
+    {
+        public DataTable Select(string query)
+        {
+            DataTable dt = new DataTable();
+            string conString = @"Data Source =(localdb)\MSSQLLocalDB; database=CrupApp;Integrated Security=True";
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                SqlDataAdapter ad = new SqlDataAdapter(query, con);
+                ad.Fill(dt);
+            }
+            return dt;
+        }
+        public void Delete(string query)
+        {
+            
+            string conString = @"Data Source =(localdb)\MSSQLLocalDB; database=CrupApp;Integrated Security=True";
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+               SqlCommand cmd =new SqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void Save(string query)
+        {
+
+            string conString = @"Data Source =(localdb)\MSSQLLocalDB; database=CrupApp;Integrated Security=True";
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
--- HomeController.cs
+++ HomeController.cs
@@ -1,34 +1,53 @@
-﻿using LoginLogout.Models;
+﻿using CrupAppNew;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace LoginLogout.Controllers
+namespace CrudAppAdo.Net.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         // GET: Home
         public ActionResult Index()
         {
+            string query = "select * from UserList";
+            var dt=new Connection();
+            var model= dt.Select(query);
+            return View("Index",model);
+        }       
+        public ActionResult Edit(int id)
+        {
+            string query = $"select * from UserList where Id={id}";
+            var dt = new Connection();
+            var model = dt.Select(query);
+            return View("Edit", model);
+        }
+        public ActionResult Delete(int id)
+        {
+            string query = $"Delete UserList where Id={id}";
+            var dt = new Connection();
+            dt.Delete(query);
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult Save()
+        {            
             return View();
         }
-        public ActionResult Save(Employee details)
+        public ActionResult SaveDetails(FormCollection frm)
         {
-
-            return Json(true);
+            string query = $"Insert Into UserList values('{frm["UserName"]}','{frm["Email"]}','{frm["Password"]}')";
+            var dt = new Connection();
+            dt.Save(query);
+            return RedirectToAction("Index","Home");
         }
-        public ActionResult GetEmployee(Employee details)
+        public ActionResult update(FormCollection frm)
         {
-            var data = new Employee()
-            {
-                Desibility = true,
-                FirstName="Rahul",
-                LastName="Kumar"
-            };
-            return Json(data);
+            string query = $"Update UserList set UserName='{frm["UserName"]}',Email='{frm["Email"]}',Password='{frm["Password"]}' where Id={frm["Id"]}";
+            var dt = new Connection();
+            dt.Save(query);
+           return RedirectToAction("Index","Home");
         }
     }
 }
